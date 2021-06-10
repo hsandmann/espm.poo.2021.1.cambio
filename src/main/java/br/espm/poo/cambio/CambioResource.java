b2558d1aaa7ba63eba4586bdebc1580a5de02cde
Humberto Sandmann <hsandmann@ieee.org>
2021-06-10 10:14:54 -0300

    Optimized imports

--- src/main/java/br/espm/poo/cambio/CambioResource.java
+++ src/main/java/br/espm/poo/cambio/CambioResource.java
@@ -5,7 +5,6 @@ import br.espm.poo.cambio.common.datatype.Cotacao;
 import br.espm.poo.cambio.common.datatype.Moeda;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +12,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 public class CambioResource implements CambioController {
